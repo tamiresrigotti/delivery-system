9f5f88d50daebf30b49997f400f1df97856ca347
tamiresrigoti <tamiresrigoti@gmail.com>
2024-02-21 19:08:45 -0300

    inicia o projeto delivery restaurant

---
+++ src/main/java/com/deliverytech/deliverysistem/DeliveryRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.deliverytech.deliverysistem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deliverytech/deliverysistem/DeliveryRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliverytech.deliverysistem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
