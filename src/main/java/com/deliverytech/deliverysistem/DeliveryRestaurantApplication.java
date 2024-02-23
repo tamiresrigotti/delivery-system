7454a1ffa2bf786ce275adbda929fa492f074493
tamiresrigoti <tamiresrigoti@gmail.com>
2024-02-23 16:22:02 -0300

    implementa api de teste

--- src/main/java/com/deliverytech/deliverysistem/DeliveryRestaurantApplication.java
+++ src/main/java/com/deliverytech/deliverysistem/DeliveryRestaurantApplication.java
@@ -2,9 +2,19 @@ package com.deliverytech.deliverysistem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/")
 @SpringBootApplication
 public class DeliveryRestaurantApplication {
+	
+	@GetMapping
+	public String getHomeTeste() {
+		return "Delivery Restaurant - API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeliveryRestaurantApplication.class, args);
