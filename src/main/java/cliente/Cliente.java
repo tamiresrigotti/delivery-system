c7bdb7e67190d5628a7f2c8baa9d1e7d37244417
tamiresrigoti <tamiresrigoti@gmail.com>
2024-02-25 09:17:07 -0300

    implementa dominiio de negocio

---
+++ src/main/java/cliente/Cliente.java
@@ -0,0 +1,51 @@
+package cliente;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Document(collection = "cliente")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Cliente {
+
+	@Id
+	private UUID idCliente;
+	@NotBlank
+	private String nomeCompleto;
+	@CPF
+	@Indexed(unique = true)
+	private String cpf;
+	@NotNull
+	private LocalDate dataDeNascimento;
+	@NotBlank
+	private Endereco endereco;
+	
+	private LocalDateTime dataHoraDoCadastro;
+	private LocalDateTime dataHoradaUltimaAlteracao;
+
+	
+	public Cliente(UUID idCliente, @NotBlank String nomeCompleto, @CPF String cpf, @NotNull LocalDate dataDeNascimento,
+			@NotBlank Endereco endereco, LocalDateTime dataHoraDoCadastro, LocalDateTime dataHoradaUltimaAlteracao) {
+		super();
+		this.idCliente = idCliente;
+		this.nomeCompleto = nomeCompleto;
+		this.cpf = cpf;
+		this.dataDeNascimento = dataDeNascimento;
+		this.endereco = endereco;
+		this.dataHoraDoCadastro = dataHoraDoCadastro;
+		this.dataHoradaUltimaAlteracao = dataHoradaUltimaAlteracao;
+	}
+
+}
---
+++ src/main/java/cliente/Endereco.java
@@ -0,0 +1,26 @@
+package cliente;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class Endereco {
+	@NotBlank
+	private String rua;
+	@NotBlank
+	private String numero;
+	@NotBlank
+	private String cidade;
+	@NotBlank
+	private String estado;
+	@NotBlank
+	private String cep;
+
+	public Endereco(@NotBlank String rua, @NotBlank String numero, @NotBlank String cidade, @NotBlank String estado,
+			@NotBlank String cep) {
+		super();
+		this.rua = rua;
+		this.numero = numero;
+		this.cidade = cidade;
+		this.estado = estado;
+		this.cep = cep;
+	}
+}
